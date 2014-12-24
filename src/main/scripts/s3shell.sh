dfc4c166996e9e023457708f9a188f2000b51cfb
Jon Svede <jsvede@yahoo.com>
2014-12-24 11:43:13 -0700

    Fixed 2 bugs in the shell script: - error message about missing urnary operator - classpath set incorrectly for the s3shell jar file

--- src/main/scripts/s3shell.sh
+++ src/main/scripts/s3shell.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ $1 == -h ];
+if [ "$1" == "-h" ]
 then
     echo Usage $0
     echo   Starts the s3sh shell for managing S3 buckets
@@ -16,6 +16,6 @@ fi
 
 export JAVA_OPTIONS="-Xms1024m -Xmx1024m -XX:NewSize=256m -XX:MaxNewSize=256m -XX:PermSize=256m -XX:MaxPermSize=256m"
 
-COMMAND="java $JAVA_OPTIONS $REMOTE_DEBUGGER -Dspring.profiles.active=s3shell.h2 -Dfile.encoding=UTF-8 -cp $PWD:$PWD/s3shell-1.0.0-SNAPSHOT-all.jar jds.s3shell.S3Shell"
+COMMAND="java $JAVA_OPTIONS $REMOTE_DEBUGGER -Dspring.profiles.active=s3shell.h2 -Dfile.encoding=UTF-8 -cp $PWD:$PWD/s3shell-0.5.0-all.jar jds.s3shell.S3Shell"
 
 $COMMAND
